5a949e941cbf6194881e23444565960b90c0d35f
Adamkadaban <adamkadaban@gmail.com>
2024-09-13 16:56:00 -0400

    wait for port to open using nc. this should prevent connection errors

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -2,6 +2,6 @@
 
 apt-get update -y
 
-apt-get install -y qemu-system-arm python3 python3-pip
+apt-get install -y qemu-system-arm python3 python3-pip netcat
 
 pip3 install -r /autograder/source/requirements.txt
