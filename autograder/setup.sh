4e192a98e51072fe0e43dab0d4842cb351d3b2e9
Adamkadaban <adamkadaban@gmail.com>
2024-10-02 15:43:36 -0400

    add jq to setup bc it helps with debugging

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -2,6 +2,6 @@
 
 apt-get update -y
 
-apt-get install -y qemu-system-arm python3 python3-pip netcat
+apt-get install -y qemu-system-arm python3 python3-pip netcat jq
 
 pip3 install -r /autograder/source/requirements.txt
